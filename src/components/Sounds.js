dc297cf91e52eb72fead162cf20d92bcf9213c08
Andy Agus <Redagus@gmail.com>
2020-12-22 10:58:25 -0500

    added sounds

--- src/components/Sounds.js
+++ src/components/Sounds.js
@@ -1,1 +1,11 @@
-// 
+import React from 'react'
+
+export default class Sounds extends React.Component{
+	render(){
+		return(
+			<div className="popup-banner">
+				<p> Sounds </p>
+			</div>
+		)
+	}
+}
